0256596e145f4de13d885c4eae970ea7799fc747
Sotev <113598164+DimSvStoev@users.noreply.github.com>
2025-04-11 11:12:39 +0300

    test branch

--- project-root/tests/ui-tests/demo.spec.ts
+++ project-root/tests/ui-tests/demo.spec.ts
@@ -4,3 +4,8 @@ test('Demo test loads example.com', async ({ page }) => {
   await page.goto('https://example.com');
   await expect(page).toHaveTitle(/Example Domain/);
 });
+
+test('Demo 2 test loads example.com', async ({ page }) => {
+  await page.goto('https://example.com');
+  await expect(page).toHaveTitle(/Example Domain/);
+});
